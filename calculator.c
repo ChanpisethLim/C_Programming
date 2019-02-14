5d784fcbaaa33596eddc7747f5e9c50f2e72b2ff
Sethsaki <45678528+Sethsaki@users.noreply.github.com>
2019-02-14 14:17:26 +0700

    Simple program to create calculator in C program

---
+++ calculator.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+main()
+{
+	char chc;
+	float a,b,c;
+	puts("+ for Addiction");
+	puts("- for Substraction");
+	puts("* for Multiplication");
+	puts("/ for Division");
+	puts("Select your choice :");
+	fflush (stdin);
+	chc = getchar();
+	printf("Enter the value for a :");
+	scanf("%f",&a);
+	printf("Enter the value for b :");
+	scanf("%f",&b);
+	switch(chc)
+	{
+		case '+' :
+			c=a+b;
+			printf("The answer is %f",c);
+			break;
+		case '-' :
+			c=a-b;
+			printf("The answer is %f",c);
+			break;
+		case '*' :
+			c=a*b;
+			printf("The answer is %f",c);
+			break;
+		case '/' :
+			c=a/b;
+			printf("The answer is %f",c);
+			break;
+		default :
+			puts("Error 404");
+	}
+}
+
