f587c04943c879f3c426b04867a7e224dd5f2326
Sokhon Chhun <47809100+SokhonC@users.noreply.github.com>
2019-03-06 10:11:40 +0700

    Update to calculator.c
    I add one more function about calculate the delta=b^2-4*a*c

--- calculator.c
+++ calculator.c
@@ -2,11 +2,12 @@
 main()
 {
 	char chc;
-	float a,b,c;
+	float a,b,c,delta;
 	puts("+ for Addiction");
 	puts("- for Substraction");
 	puts("* for Multiplication");
 	puts("/ for Division");
+	puts("$ for findind the delta");
 	puts("Select your choice :");
 	fflush (stdin);
 	chc = getchar();
@@ -31,6 +32,12 @@ main()
 		case '/' :
 			c=a/b;
 			printf("The answer is %f",c);
+			break;
+		case '$':
+			printf("Enter the value for c :");
+			scanf("%f",&c);
+			delta=b*b-(4*a*c);
+			printf("The answer of delta is %f",delta);
 			break;
 		default :
 			puts("Syntax Error");
