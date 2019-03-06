25eb6e0817773125585f3edad4a488d8302eccdb
Sokhon Chhun <47809100+SokhonC@users.noreply.github.com>
2019-03-06 11:26:13 +0700

    Update this repository again lol
    You can test it

--- calculator.c
+++ calculator.c
@@ -3,13 +3,14 @@
 main()
 {
 	char chc;
-	float a,b,c;
+	float a,b,c,delta;
 	puts("+ for Addiction");
 	puts("- for Substraction");
 	puts("* for Multiplication");
 	puts("/ for Division");
 	puts("^ for power");
 	puts("@ for radical calculation");
+	puts("$ for findind the delta");
 	puts("Select your choice :");
 	fflush (stdin);
 	chc = getchar();
@@ -66,6 +67,12 @@ main()
 			scanf("%f", &b);
 			c=pow(a,1/b);
 			printf("The answer is %f", c);
+			break;
+		case '$':
+			printf("Enter the value for c :");
+			scanf("%f",&c);
+			delta=b*b-(4*a*c);
+			printf("The answer of delta is %f",delta);
 			break;
 		default :
 			puts("Syntax Error");
